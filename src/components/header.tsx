f38878a8f48f9f781304be68b00ebe9e39994687
Alice-PDA <mariaaliceifba@gmail.com>
2025-07-17 09:44:44 -0300

    :recycle: refactor: Move components folder into src

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import { useState } from "react"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/src/components/ui/button"
 import { Heart, Menu, X } from "lucide-react"
 import { usePathname } from "next/navigation"
 
